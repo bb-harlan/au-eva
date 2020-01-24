a5ab149bb3cf879bc838dcde3bbbb43c4eff2db1
Alan Anderson <anderson1941@sbcglobal.net>
2020-01-24 14:50:17 -0600

    chore: Move back to docs folder for dist output.
    `docs` is a special folder for use with github pages and cannot be renamed.

--- package-scripts.js
+++ package-scripts.js
@@ -74,6 +74,6 @@ module.exports = {
         hmr: `webpack-dev-server -d --devtool '#source-map' --inline --hot --env.server`
       },
     },
-    serve: 'http-server dist --cors',
+    serve: 'http-server docs --cors',
   },
 }
